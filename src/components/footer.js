da030b4a86a3b3b1faafc44d043773ab3f11bfe1
Leo Toikka <leo@seravo.fi>
2018-09-29 20:11:12 +0300

    Add links for React.js and WP logos in footer technology section

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,7 +15,14 @@ export default class Footer extends Component {
           </a> {(new Date()).getFullYear()}
         </div>
         <div className="footer-ps-section">
-          <span>Powered by</span> <img src={ReactLogo} alt="React"></img> <span>&</span> <img src={WPLogo} alt="WordPress"></img>
+          <span>Powered by</span>
+          <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
+            <img src={ReactLogo} alt="React"></img>
+          </a>
+          <span>&</span>
+          <a href="https://wordpress.org/" target="_blank" rel="noopener noreferrer">
+            <img src={WPLogo} alt="WordPress"></img>
+          </a>
         </div>
       </div>
     );
